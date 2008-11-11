f5945279e479574e26099e25041ffad0d737c7fb
Natan Yellin <aantny@gmail.com>
2008-11-11 21:51:28 +0200

    Added SpiffGtkWidgets.Calendar based calendar view.  * Added SpiffGtkWidgets library. This was imported directly from r19 from the project's svn repository. Please see the projects website at http://code.google.com/p/spiff-gtkwidgets/  * Added a quick calendar gui using SpiffGtkWigets.Calendar.Calendar. It still doesn't show anything interesting and is disabled by default. To enable it, please follow the instructions in src/start.py

---
+++ src/mayanna_gui/mayanna_calendar_gui.py
@@ -0,0 +1,87 @@
+import datetime
+import math
+import os
+import sys
+import time
+
+sys.path.append("libs/")
+from SpiffGtkWidgets import Calendar
+import gnomeapplet
+import gobject
+import gtk
+import gtk.glade
+    
+from mayanna_panel_widgets import MayannaWidget
+from mayanna_engine.mayanna_util import icon_factory, icon_theme, launcher
+
+
+class MayannaGUI:   
+    
+    '''   Initilization   '''
+   
+    def __init__(self):
+        self.date = time.localtime()
+        self.create_gui()
+        
+    def create_gui(self):
+        '''
+        Main Window holding everything inside it
+        '''
+        self.topicWindow = gtk.Window()
+        self.topicWindow.set_title("Timeline")
+        self.topicWindow.resize(700, 500)
+        self.topicWindow.set_resizable(True)
+        self.topicWindow.set_border_width(1)
+        self.topicWindow.show_all()
+        if (self.topicWindow):
+            self.topicWindow.connect("destroy", gtk.main_quit)
+        
+        '''
+        Paned view containing a sidebar and the calendar
+        '''
+        paned = gtk.HPaned()
+        self.topicWindow.add(paned)
+        
+        '''
+        Sidebar
+        '''
+        vbox = gtk.VBox()
+        paned.add1(vbox)
+        
+        self.miniCalendar = gtk.Calendar()
+        self.miniCalendar.select_month(self.date[1], self.date[0])
+        self.miniCalendar.set_display_options(gtk.CALENDAR_SHOW_HEADING)
+        vbox.add(self.miniCalendar)
+        
+        '''Main View'''
+        vbox = gtk.VBox(False)
+        paned.add2(vbox)
+        
+        buttonBox = gtk.HButtonBox()
+        vbox.pack_start(buttonBox, False)
+        
+        for buttonText in ["Week", "Month"]:
+            button = gtk.Button(buttonText)
+            button.connect("clicked",
+                self.on_mode_button_clicked, buttonText)
+            buttonBox.add(button)
+            
+        self.calendarModel = Calendar.Model()
+        self.calendar = Calendar.Calendar(self.calendarModel)
+        self.calendar.set_range(Calendar.Calendar.RANGE_WEEK)
+        vbox.add(self.calendar)
+        
+        #self.mayannapanel = MayannaWidget()
+        #self.mayannapanel.show_all()
+        #self.mainTable.pack_start(self.mayannapanel,True,True)
+        self.topicWindow.show_all()
+    
+    def on_mode_button_clicked (self, button, buttonLabel):
+        '''
+        Called when one of the buttons to change
+        the calendar's mode has been clicked.
+        '''
+        if buttonLabel == "Week":
+            self.calendar.set_range(Calendar.Calendar.RANGE_WEEK)
+        else:
+            self.calendar.set_range(Calendar.Calendar.RANGE_MONTH)
--- src/start.py
+++ src/start.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
 import sys
+
 import gtk
+
+# uncomment the following line to use the new calendar gui
+#from mayanna_gui.mayanna_calendar_gui import MayannaGUI
+
+# comment out the following line if you're using the new calendar gui
 from mayanna_gui.mayanna_gui import MayannaGUI
+
  
 if __name__ == "__main__":
     gui=MayannaGUI()
-    gtk.main()       
+    gtk.main()
