cfc0d3edd92d1f75765098280b442571ce2b047f
Natan Yellin <aantny@gmail.com>
2008-11-12 21:34:23 +0200

    Minor improvement to the calendar interface.  * Added a label to display the current month and year and hooked it up to the necessary code.

--- src/mayanna_gui/mayanna_calendar_gui.py
+++ src/mayanna_gui/mayanna_calendar_gui.py
@@ -18,7 +18,20 @@ from mayanna_engine.mayanna_util import icon_factory, icon_theme, launcher
 class MayannaGUI:   
     
     '''   Initilization   '''
-   
+    
+    month_mapping = ["January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July"
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"]
+        
     def __init__(self):
         self.date = time.localtime()
         self.create_gui()
@@ -26,6 +39,13 @@ class MayannaGUI:
     def create_gui(self):
         self.glade = gtk.glade.XML("calendar.glade")
         self.glade.signal_autoconnect(self)
+        
+        self.monthLabel = self.glade.get_widget("monthLabel")
+        self.monthLabel.set_markup("<b>%s %s</b>" % 
+            (self.month_mapping[self.date[1]-1], self.date[0]))
+        
+        self.miniCalendar = self.glade.get_widget("miniCalendar")
+        self.miniCalendar.select_month(self.date[1], self.date[0])
         
         self.calendarModel = Calendar.Model()
         self.calendar = Calendar.Calendar(self.calendarModel)
