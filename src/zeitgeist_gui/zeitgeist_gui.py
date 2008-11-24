4f8badbe73842545abc1bf453a1dd339abd0297b
Natan Yellin <aantny@gmail.com>
2008-11-24 23:32:54 +0200

    Minor fixes in zeitgeist_gui.py:  * Only show the toplevel window after it's children widgets have been added so that everything shows up at once.  * Fixes spacing.  * Removed crud.

--- src/zeitgeist_gui/zeitgeist_gui.py
+++ src/zeitgeist_gui/zeitgeist_gui.py
@@ -12,13 +12,9 @@ from zeitgeist_panel_widgets import timeline,StarredWidget,FilterAndOptionBox,ca
 from zeitgeist_engine.zeitgeist_util import icon_factory, icon_theme, launcher
 #from zeitgeist_calendar_gui import zeitgeistGUI
 
-class zeitgeistGUI:   
-	
-	'''   Initilization   '''
+class zeitgeistGUI:
    
 	def __init__(self):
-		
-		#self.main_btn =  gtk.Button("Start")
 		self.create_gui()
 		
 	def create_gui(self):
@@ -31,40 +27,36 @@ class zeitgeistGUI:
 		self.topicWindow.set_title("Gnome Zeitgeist")
 		self.topicWindow.set_resizable(True)
 		self.topicWindow.set_border_width(5)
-		self.topicWindow.show_all()
-		if (self.topicWindow):
-			self.topicWindow.connect("destroy", gtk.main_quit)
+		self.topicWindow.connect("destroy", gtk.main_quit)
 
-		self.mainbox=gtk.VBox()
+		self.mainbox = gtk.VBox()
 		self.mainTable = gtk.HBox()    
 		self.sidebar = gtk.VBox()
+		
 		''' 
 		HeaderTable
-		
 		'''
 		
-		self.starredbox=StarredWidget()
+		self.starredbox = StarredWidget()
 		self.notebook = gtk.Notebook()
-		self.faobox= FilterAndOptionBox()
+		self.faobox = FilterAndOptionBox()
 		
 		
-		self.sidebar.pack_start(calendar,False,False)
-		self.sidebar.pack_start(self.faobox,True,True)
+		self.sidebar.pack_start(calendar, False, False)
+		self.sidebar.pack_start(self.faobox, True, True)
 		
 		
-		self.notebook.append_page(self.starredbox,gtk.Label("Starred"))
-		self.notebook.append_page(timeline,gtk.Label("Timeline"))
+		self.notebook.append_page(self.starredbox, gtk.Label("Starred"))
+		self.notebook.append_page(timeline, gtk.Label("Timeline"))
 		
-		self.mainTable.pack_start(self.sidebar,False,False,5)
-		self.mainTable.pack_start(self.notebook,True,True,5)
+		self.mainTable.pack_start(self.sidebar, False, False,5)
+		self.mainTable.pack_start(self.notebook, True, True,5)
 	
-		self.mainbox.pack_start(self.mainTable,True,True,5)
+		self.mainbox.pack_start(self.mainTable, True, True,5)
 		
 		statusbar = gtk.Statusbar()
-		self.mainbox.pack_start(statusbar,False,False)
+		self.mainbox.pack_start(statusbar, False, False)
 		
 		
 		self.topicWindow.add(self.mainbox)
 		self.topicWindow.show_all()
-		
-	
