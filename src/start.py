d7615a67e483b25c4279f059cdea6b50de1b4f12
Natan Yellin <aantny@gmail.com>
2008-11-24 18:29:29 +0200

    Removed traces of the calendar GUI from start.py.

--- src/start.py
+++ src/start.py
@@ -4,12 +4,7 @@ import sys
 
 import gtk
 
-# uncomment the following line to use the new calendar gui
-#from zeitgeist_gui.zeitgeist_calendar_gui import zeitgeistGUI
-
-# comment out the following line if you're using the new calendar gui
 from zeitgeist_gui.zeitgeist_gui import zeitgeistGUI
-
  
 if __name__ == "__main__":
     gui=zeitgeistGUI()
