2058286eff46f04ca1c8567ba48b21673729973b
Natan Yellin <aantny@gmail.com>
2009-01-14 21:26:17 +0200

    More readability fixes.

--- src/zeitgeist_engine/zeitgeist_dbcon.py
+++ src/zeitgeist_engine/zeitgeist_dbcon.py
@@ -12,7 +12,6 @@ from zeitgeist_engine.zeitgeist_base import DataProvider, Data
 class DBConnector:
 	
 	def __init__(self):
-		
 		self.create_db()
 		path = glob.glob(os.path.expanduser("~/.Zeitgeist/gzg.sqlite"))
 		self.connection = sqlite3.connect(path[0], True)
@@ -30,20 +29,18 @@ class DBConnector:
 				except:
 					pass
 				shutil.copy("gzg.sqlite", dbdir)	  
-			except :
-				print "Unexpected error creating database:", sys.exc_info()[0]
-
+			except:
+				print "Unexpected error creating database:", sys.exc_info()[0]	
 	
-
 	def get_last_timestmap(self):
-		temp = self.cursor.execute( "SELECT * FROM timetable WHERE start IN (SELECT MAX(start) AS start FROM timetable)").fetchall()
+		query = "SELECT * FROM timetable WHERE start IN (SELECT MAX(start) AS start FROM timetable)"
+		result = self.cursor.execute(query).fetchall()
 		try:
-			return temp[0][0]
+			return result[0][0]
 		except:
 			return 0
 		
-		
-	def insert_items(self,items):
+	def insert_items(self, items):
 		for item in items:
 				try:
 					self.cursor.execute('INSERT INTO timetable VALUES (?,?,?,?)', (item.timestamp,
@@ -59,21 +56,17 @@ class DBConnector:
 																						item.count,
 																						item.use,
 																						item.type))
-						print("wrote "+item.uri+" into database")
+						print "Wrote %s into the database." % item.uri
 					except:
-						
-						print "Error writing " + item.uri + " with timestamp "+ str(item.timestamp)
-				except:
+						print "Error writing %s with timestamp %s." %(item.uri, item.timestamp)
+				except Exception, ex:
 					pass
-				
-				del item
-		del items
+					#print "Error: %s" % ex 
 		self.connection.commit()
 		   
 	def get_items(self,min,max):
-
-		for t in  self.cursor.execute("SELECT start , end,  uri FROM timetable WHERE start >= "+str(int(min)) +" and start <= " + str(int(max))).fetchall():
-			
+		for t in self.cursor.execute("SELECT start , end,  uri FROM timetable WHERE start >= "
+									+ str(int(min)) + " and start <= " + str(int(max))).fetchall():
 			i = self.cursor.execute("SELECT * FROM data WHERE uri=?",(t[2],)).fetchone()
 			try:
 				yield Data(uri=i[0], 
@@ -87,24 +80,19 @@ class DBConnector:
 					          type=i[7])
 			except:
 				print "ERROR"
-				#pass
-			del i,t
 		gc.collect()
 	 
 	def update_item(self,item):
-		 
 		self.cursor.execute('DELETE FROM  data where uri=?',(item.uri,))
 		self.cursor.execute('INSERT INTO data VALUES (?,?,?,?,?,?,?,?)',(item.uri,
-																								   item.name,
-																								   item.comment,
-																								   item.mimetype,
-																								   item.tags,
-																								   item.count,
-																								   item.use,
-																								   item.type))
-		
-		
-	 	self.cursor.execute('DELETE FROM tags where uri=?',(item.uri,))
+																		item.name,
+																		item.comment,
+																		item.mimetype,
+																		item.tags,
+																		item.count,
+																		item.use,
+																		item.type))		
+	 	self.cursor.execute('DELETE FROM tags where uri=?', (item.uri,))
 	 	 
 		for tag in item.get_tags():
 			if not tag.strip() == "":
