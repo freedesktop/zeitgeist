663aa8f4b264146f3c6914f12a73dacca27d1a5a
Natan Yellin <aantny@gmail.com>
2009-01-17 22:51:06 +0200

    Cleaned up zeitgeist_dbcon.py.

--- src/zeitgeist_engine/zeitgeist_dbcon.py
+++ src/zeitgeist_engine/zeitgeist_dbcon.py
@@ -8,6 +8,28 @@ import gc
 import os
 from zeitgeist_engine.zeitgeist_base import DataProvider, Data
 
+# Constants for quick use in SQL queries:
+# - Rows in the data table
+D_URI		= 0
+D_NAME		= 1
+D_COMMENT	= 2
+D_MIMETYPE	= 3
+D_TAGS		= 4
+D_COUNT		= 5
+D_USE		= 6
+D_TYPE		= 7
+
+# - Rows in the tags table
+TAG = 0
+TAG_URI = 1
+
+# - Rows in the timetable table
+TIMESTAMP_BEGIN		= 0
+TIMESTAMP_END		= 1
+TIMESTAMP_URI		= 2
+TIMESTAMP_DIFF		= 3
+
+
 class DBConnector:
 	
 	def __init__(self):
@@ -32,11 +54,11 @@ class DBConnector:
 	
 	def get_last_timestamp(self):
 		query = "SELECT * FROM timetable LIMIT 1"
-		result = self.cursor.execute(query).fetchall()
-		try:
-			return result[0][0]
-		except:
+		result = self.cursor.execute(query).fetchone()
+		if result is None:
 			return 0
+		else:
+			return result[TIMESTAMP_BEGIN]
 		
 	def insert_items(self, items):
 		for item in items:
@@ -65,16 +87,16 @@ class DBConnector:
 	def get_items(self,min,max):
 		for t in self.cursor.execute("SELECT start , end,  uri FROM timetable WHERE start >= "
 									+ str(int(min)) + " and start <= " + str(int(max))).fetchall():
-			i = self.cursor.execute("SELECT * FROM data WHERE uri=?",(t[2],)).fetchone()
-			yield Data(uri=i[0], 
-					  timestamp= t[0], 
-					  name= i[1], 
-					  comment=i[2], 
-					  mimetype=  i[3], 
-					  tags=i[4], 
-					  count=i[5], 
-					  use =i[6], 
-					  type=i[7])
+			i = self.cursor.execute("SELECT * FROM data WHERE uri=?",(t[TIMESTAMP_URI],)).fetchone()
+			yield Data(uri=i[D_URI], 
+					  timestamp= t[TIMESTAMP_BEGIN],
+					  name= i[D_NAME],
+					  comment=i[D_COMMENT],
+					  mimetype=  i[D_MIMETYPE],
+					  tags=i[D_TAGS],
+					  count=i[D_COUNT],
+					  use =i[D_USE],
+					  type=i[D_TYPE])
 			gc.collect()
 	 
 	def update_item(self,item):
