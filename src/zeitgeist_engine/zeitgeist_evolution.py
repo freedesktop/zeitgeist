1adca826d8987519a1b0d7f14afec76ab925c396
Seif Lotfy <seif@Schroeder>
2009-02-05 18:44:20 +0100

    crash proof evo plugin

--- src/zeitgeist_engine/zeitgeist_evolution.py
+++ src/zeitgeist_engine/zeitgeist_evolution.py
@@ -55,6 +55,10 @@ class EvolutionSource(DataProvider):
 		j = 0
 		for i in history:
 			if i != None:
+				if i[1]==None:
+					i[1]==""
+				if i[2] == None:
+					i[2] ==""
 				name = i[1]+" \n"+i[2]
 				timestamp = i[0] 
 				yield Data(uri="mailto:"+i[2],
