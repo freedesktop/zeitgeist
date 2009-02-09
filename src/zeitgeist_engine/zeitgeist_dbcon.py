38823ec8caa7a5e8919271c8af085b6dc5371be2
Seif Lotfy <seif@Schroeder>
2009-02-09 23:31:43 +0100

    some minor improvments

--- src/zeitgeist_engine/zeitgeist_dbcon.py
+++ src/zeitgeist_engine/zeitgeist_dbcon.py
@@ -159,41 +159,42 @@ class DBConnector:
 		list ={}
 		while relevant != []:
 			r = relevant[0]
-			x =  relevant.count(r)
+			x =  1
 			heat=0
 			while relevant.count(r)>0:
 				index = relevant.index(r)
+				if relevant2[index]>heat:
+					heat=relevant2[index]
+					x+=1
 				relevant.pop(index)
-				heat=heat+relevant2[index]
 				relevant2.pop(index)
 				
 			if r  != item.uri:
-				list[r]=[x,heat/x]
+				list[r]=[x,heat]
 
 		values = [(v, k) for (k, v) in list.iteritems()]
 		list.clear()
 		values.sort()
 		values.reverse()
 		
-		for v in values:
-			print v
 		
-		for index in range(7):
+		types=[]
+		items=[]
+		for index in range(len(values)):
 			try:
 				uri = values[index][1]
 				i = self.cursor.execute("SELECT * FROM data WHERE uri=?",(uri,)).fetchone() 
 				if i:
-					yield Data(uri=i[0], 
-					  timestamp= -1.0, 
-					  name= i[1], 
-					  comment=i[2], 
-					  mimetype=  i[3], 
-					  tags=i[4], 
-					  count=i[5], 
-					  use =i[6], 
-					  type=i[7])
+					if types.count(i[7]) <= 10:
+						print "TYPE"
+						d= Data(uri=i[0],timestamp= -1.0, name= i[1], comment=i[2], mimetype=  i[3], tags=i[4], count=i[5], use =i[6], type=i[7])
+						types.append(i[7])
+						items.append(d)
+					else:
+						print "ERROR"
 			except:
 				pass
+		return items
 		
 		
 	def numeric_compare(x, y):
