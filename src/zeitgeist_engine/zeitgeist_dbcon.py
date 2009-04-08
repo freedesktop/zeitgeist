61759c3c3df84ede350f7a05c9bdc9da674bfa99
Siegfried-Angel Gevatter Pujals <rainct@ubuntu.com>
2009-04-08 17:36:38 +0200

    Make it possible to add tags with unicode characters. (btw, unicode support is badly broken everywhere).

--- src/zeitgeist_engine/zeitgeist_dbcon.py
+++ src/zeitgeist_engine/zeitgeist_dbcon.py
@@ -123,15 +123,15 @@ class DBConnector():
 																		item.name,
 																		item.comment,
 																		item.mimetype,
-																		item.tags,
+																		unicode(item.tags),
 																		item.count,
 																		item.use,
 																		item.type,
 																		item.icon,
 																		bookmark))	
-			
+		
 		self.cursor.execute('DELETE FROM tags where uri=?', (item.uri,))
-		 
+		
 		for tag in item.get_tags():
 			if not tag.strip() == "":
 				try:
@@ -139,7 +139,7 @@ class DBConnector():
 				except:
 					pass
 				#id = self.cursor.execute("SELECT rowid FROM tagids WHERE  tag=?",(tag,)).fetchone()
-				self.cursor.execute('INSERT INTO tags VALUES (?,?,?)',(tag.capitalize(),item.uri,time.time())) 	
+				self.cursor.execute('INSERT INTO tags VALUES (?,?,?)',(unicode(tag.capitalize()),item.uri,time.time())) 	
 		self.connection.commit()
 		 
 	def get_recent_tags(self,count=20,min=0,max=sys.maxint):
