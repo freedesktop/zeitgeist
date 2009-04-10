6da9170ec6a56cb5e331dc4e2c8aa588707ee14b
Natan Yellin <aantny@gmail.com>
2009-04-10 13:22:46 +0300

    Cleaned up Twitter data provider.

--- src/zeitgeist_engine/zeitgeist_twitter.py
+++ src/zeitgeist_engine/zeitgeist_twitter.py
@@ -1,20 +1,21 @@
 from zeitgeist_base import Data, DataProvider
-import twitter
 
-class TwitterData(Data):
-	def __init__(self,tweet):
-		self.uri = "http://explore.twitter.com/" + tweet.user.screen_name + "/status/" + str(tweet.id)
-		self.name = tweet.user.name + ":\n" + tweet.text 
-		self.timestamp = tweet.created_at_in_seconds
-		self.icon = None
-		Data.__init__(self, name=self.name, uri=self.uri,timestamp=self.timestamp, count=0, use="tweet", type="Twitter")
-		
+try:
+	import twitter
+except ImportError:
+	twitter = None
+	print "Twitter support is disabled. Please install python-twitter to enable it."
+
 class TwitterSource(DataProvider):
+	"""
+	Indexes Twitter statuses.
+	"""
 	
 	def __init__(self):
+		# TODO: Store the user's username and password using GNOME Keychain.
 		DataProvider.__init__(self, uri="gzg/twitter", name="Twitter")
-		self.username = ''
-		self.password = ''
+		self.username = ""
+		self.password = ""
 		self.comment = " tweets to Twitter"
 		
 	def get_icon_static(self,icon_size):
@@ -22,10 +23,15 @@ class TwitterSource(DataProvider):
 		self.icon = gtk.gdk.pixbuf_new_from_file_at_size(loc[0], -1, int(24))
 		
 	def get_items_uncached(self):
-		try:
-			self.api = twitter.Api(username=self.username, password=self.password)
-			for status in self.api.GetUserTimeline(count = 500):
-				yield TwitterData(status)
-		except:
-			pass
-			
+		# If twitter isn't installed or if we don't have a username and password
+		if twitter is None or username=="" or password="":
+			return
+		
+		# Connect to twitter, loop over statuses, and create items for each status
+		self.api = twitter.Api(username=self.username, password=self.password)
+		for status in self.api.GetUserTimeline(count = 500):
+			yield Data(type = "Twitter",
+						icon = None, count=0, use="tweet",
+						name = tweet.user.name + ":\n" + tweet.text,
+						timestamp = tweet.created_at_in_seconds,
+						uri = "http://explore.twitter.com/" + tweet.user.screen_name + "/status/" + str(tweet.id))
