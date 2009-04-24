ffdac359440bd5fd48b9dd2f79c8b91161d888a3
Siegfried-Angel Gevatter Pujals <rainct@ubuntu.com>
2009-04-24 23:46:58 +0200

    Global exceptions are the evil

--- src/zeitgeist_engine/zeitgeist_firefox.py
+++ src/zeitgeist_engine/zeitgeist_firefox.py
@@ -62,40 +62,37 @@ class FirefoxSource(DataProvider):
         self.emit("reload")
     
     def get_items_uncached(self):
+        # create a connection to firefox's sqlite database
+        
+        # retrieve all urls from firefox history
+        contents = "id, place_id, visit_date,visit_type"
         try:
-            # create a connection to firefox's sqlite database
-            
-            # retrieve all urls from firefox history
-            contents = "id, place_id, visit_date,visit_type"
-            try:
-                history = self.cursor.execute("SELECT " + contents + " FROM moz_historyvisits WHERE visit_date>?",(self.last_timestamp,)).fetchall()
-            except db.OperationalError, e:
-                print 'Firefox database error:', e
-            else:
-                for j, i in enumerate(history):
-                    # TODO: Fetch full rows above so that we don't need to do another query here
-                    contents = "id, url, title, visit_count, rev_host"
-                    item = self.cursor.execute("SELECT " + contents +" FROM moz_places WHERE title!='' and id=" + str(i[1])).fetchone()
-                    if item:
-                        self.last_timestamp = history[j][2]
-                        use = "linked"
-                        if history[j][3] in (2, 3, 5):
-                            use = "visited"
-                        item = {
-                            "timestamp": int(self.last_timestamp / (1000000)),
-                            "uri": unicode(item[1]),
-                            "name": unicode(item[2]),
-                            "comment": unicode(item[4][::-1]),
-                            "type": u"Firefox History",
-                            "count": item[3],
-                            "use": unicode(use),
-                            "mimetype": u"", # TODO: Can we get a mime-type here?
-                            "tags": u"",
-                            "icon": u"gnome-globe"
-                            }
-                        yield item
-        except Exception, ex:
-            print ex
+            history = self.cursor.execute("SELECT " + contents + " FROM moz_historyvisits WHERE visit_date>?",(self.last_timestamp,)).fetchall()
+        except db.OperationalError, e:
+            print 'Firefox database error:', e
+        else:
+            for j, i in enumerate(history):
+                # TODO: Fetch full rows above so that we don't need to do another query here
+                contents = "id, url, title, visit_count, rev_host"
+                item = self.cursor.execute("SELECT " + contents +" FROM moz_places WHERE title!='' and id=" + str(i[1])).fetchone()
+                if item:
+                    self.last_timestamp = history[j][2]
+                    use = "linked"
+                    if history[j][3] in (2, 3, 5):
+                        use = "visited"
+                    item = {
+                        "timestamp": int(self.last_timestamp / (1000000)),
+                        "uri": unicode(item[1]),
+                        "name": unicode(item[2]),
+                        "comment": unicode(item[4][::-1]),
+                        "type": u"Firefox History",
+                        "count": item[3],
+                        "use": unicode(use),
+                        "mimetype": u"", # TODO: Can we get a mime-type here?
+                        "tags": u"",
+                        "icon": u"gnome-globe"
+                        }
+                    yield item
     
     def __copy_sqlite(self):
         '''
