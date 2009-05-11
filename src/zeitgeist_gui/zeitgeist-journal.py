6bfcb53d944ffd1ac4aeda7c09fd16b3e1ffd599
Seif Lotfy <seif@lotfy.com>
2009-05-11 20:41:11 +0200

    clean up

--- src/zeitgeist_gui/zeitgeist-journal.py
+++ src/zeitgeist_gui/zeitgeist-journal.py
@@ -23,22 +23,12 @@ class Journal(gtk.Window):
 		# Window
 		self.set_title("GNOME Zeitgeist")
 		self.set_resizable(True)
-		#self.set_default_size(800, -1)
+		self.set_default_size(800, -1)
 		self.set_icon_from_file("%s/data/logo/scalable/apps/gnome-zeitgeist.svg" % BASEDIR)
-		
 		self.connect("destroy", gtk.main_quit)
 		self.connect('check-resize', self.window_state_event_cb)
+		self.connect("key-press-event",self.on_window_key_press_event)
 		signal.signal(signal.SIGUSR1, lambda *discard: self.emit(gtk.main_quit))
-		
-		# Vertical box (contains self.hBox and a status bar)
-		self.vBox = gtk.VBox(False, 5)
-		self.vBox.pack_start(bb, False, False)
-		self.add(self.vBox)
-		
-		# Horizontal box (contains the main content and a sidebar)
-		self.hBox = gtk.HBox()
-		self.vBox.pack_start(self.hBox, True, True,1)
-		self.hBox.set_border_width(5)
 		
 		# Sidebar
 		self.sidebar = gtk.VBox()
@@ -58,8 +48,8 @@ class Journal(gtk.Window):
 		self.notebook = gtk.Notebook()
 		self.notebook.connect("switch-page",self.switch_page)
 		self.notebook.set_homogeneous_tabs(True)
-		#notebook.set_property("tab-pos", gtk.POS_LEFT)
 		
+		# Notebook components
 		journal = "%s/data/calendar.svg" % BASEDIR
 		label = self.create_tab_label(_("Journal"), journal)
 		self.notebook.append_page(evbox, label)
@@ -73,49 +63,53 @@ class Journal(gtk.Window):
 		box = gtk.VBox()
 		self.notebook.append_page(box, gtk.Label("Most Used Stuff (not yet  implemented)"))
 		self.notebook.set_tab_label_packing(box, True, True, gtk.PACK_START)
-		#notebook.set_tab_label_packing(bookmarks, True, True, gtk.PACK_START)
-		#notebook.set_tab_label_packing(evbox, True, True, gtk.PACK_START)
-		
-		self.connect("key-press-event",self.on_window_key_press_event)
-
-		
-		# vbox for timeline and tagbar
-		vbox = gtk.VBox()
-		vbox.pack_start(self.notebook, True, True)
-		#vbox.pack_start(tagbox,False,True,2)
-		
-		hbox = gtk.HBox()
-		self.hBox.pack_start(htb,False,True,2)
-		hbox.pack_start(vbox,True,True,1)
-		
-		self.hBox.pack_start(hbox, True, True,5)
-		self.hBox.pack_start(self.sidebar, False, False)
 		
 		# Status bar
 		statusbar = gtk.Statusbar()
+		
+		
+		
+		# Vertical box (contains self.hBox and a status bar)
+		self.vBox = gtk.VBox(False, 5)
+		self.vBox.pack_start(bb, False, False)
+		self.add(self.vBox)
+		
+		# Horizontal box (contains the main content and a sidebar)
+		self.hBox = gtk.HBox()
+		self.vBox.pack_start(self.hBox, True, True,1)
+		self.hBox.set_border_width(5)
+		self.hBox.pack_start(htb,False,True,2)
+		self.hBox.pack_start(self.notebook,True,True,1)
+		self.hBox.pack_start(self.sidebar, False, False)
 		self.vBox.pack_start(statusbar, False, False)
 		
 		# Show everything
 		self.show_all()
-		#bookmarks.hide_all()
 		htb.hide_all()
 		filtersBox.option_box.hide_all()
 		calendar.hide_all()
 		
 		self.set_focus(None)
 		
-		#self.window_state_event_cb(None)
+		
+	'''
+	Check which tab is active and bind the keys event to it
+	'''
 	def on_window_key_press_event(self,timelime,event):
 		if self.notebook.get_current_page() == 0:
 			if event.keyval==65360:
 				timeline.jump_to_day(str(datetime.datetime.today().strftime("%d %m %Y")).split(" "))
 				self.set_focus(None)
+				
+			# KEY == LEFT
 			if event.keyval==65361:
 				timeline.step_in_time(-1)
-	
 				timeline.get_dayboxes()[1][1].grab_focus()
+			
+			# KEY == Right
 			if event.keyval==65363:
 				timeline.step_in_time(+1)
+				timeline.get_dayboxes()[1][1].grab_focus()
 		
 	def switch_page(self, notebook, page, page_num):	
 		if page_num == 1 or page_num ==2:
@@ -124,25 +118,24 @@ class Journal(gtk.Window):
 			bb.set_time_browsing(True)
 			
 	def create_tab_label(self, title, stock):
-			box = gtk.HBox()
-			
-			pixbuf = icon_factory.load_icon(stock, icon_size = 16 ,cache = False)
 			icon = gtk.Image()
-			icon.set_from_pixbuf(pixbuf)
-			del pixbuf
+			icon = gtk.image_new_from_pixbuf(icon_factory.load_icon(stock, icon_size = 16 ,cache = False))
 
 			label = gtk.Label(title)
 			
+			
+			box = gtk.HBox()	
 			box.pack_start(icon, False, False)
 			box.pack_start(label, True, True)
 			box.show_all()
+			
+			del label
+			del icon
 			return box
 
 	def window_state_event_cb(self, window):
 		
 		width, height = self.get_size()
-		
-		#bookmarks.bookmarks.view.reload_name_cell_size(width-50)
 		
 		if width < 800:
 			self.set_size_request(800,-1)
