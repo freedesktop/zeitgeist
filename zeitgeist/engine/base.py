6c97d5f3aff774442d04cc29f8380c1192c9bb07
Seif Lotfy <seif@lotfy.com>
2009-06-06 23:04:21 +0200

    create table instead of taking it with us (only for new DB)

--- zeitgeist/engine/base.py
+++ zeitgeist/engine/base.py
@@ -21,28 +21,82 @@ class URI(object):
     id = Int(primary=True)
     value = Unicode()
     
+    
+
 class Item(object):
     __storm__table__= "item"
-    id = URI(primary=True)
     content = Content()
     source = Source()
     caption = Unicode()
     mimetype = Unicode()
-    icon = Unicode()
-   # payload =
+    icon_name = Unicode()
+    icon_type = Unicode()
+    #payload = ByteArray()
    
 class App(Item):
     __storm__table__= "app"
     info = Unicode()
-   # payload =
    
 class Annotation(Item):
     __storm__table__= "annotation"
     target = Unicode()
-   # payload =
    
 class Event(Item):
+
+    __storm__table__= "event"
     subject = Int()
     start = Int()
     end = Int()
     app = Int()
+    
+
+database = create_database("sqlite:/home/seif/Desktop/test.sqlite")
+store = Store(database)
+try:
+    store.execute("CREATE TABLE content" 
+              "(id INTEGER PRIMARY KEY, value VARCHAR)")
+except:
+    pass
+
+try:
+    store.execute("CREATE TABLE source" 
+              "(id INTEGER PRIMARY KEY, value VARCHAR)")
+except:
+    pass
+
+try:
+    store.execute("CREATE TABLE uri" 
+              "(id INTEGER PRIMARY KEY, value VARCHAR)")
+except:
+    pass
+
+try:
+    store.execute("CREATE TABLE item" 
+              "(id INTEGER PRIMARY KEY, content INTEGER, source INTEGER, caption VARCHAR, mimetype VARCHAR, icon_name VARCHAR, icon_type VARCHAR, payload BLOB)")
+except:
+    pass
+
+try:
+    store.execute("CREATE TABLE app" 
+              "(id INTEGER PRIMARY KEY, value VARCHAR)")
+except:
+    pass
+
+try:
+    store.execute("CREATE TABLE annotation" 
+              "(id INTEGER PRIMARY KEY, value VARCHAR)")
+except:
+    pass
+
+try:
+    store.execute("CREATE TABLE event" 
+              "(id INTEGER PRIMARY KEY, subject INTEGER, start INTEGER, end INTEGER, app INTEGER")
+except:
+    pass
+
+
+
+
+store.commit()
+
+
