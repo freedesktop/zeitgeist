fc5bff558e56a57360985a0d3b59420603a9c2cd
Siegfried-Angel Gevatter Pujals <rainct@ubuntu.com>
2009-06-10 17:20:45 +0200

    Add proper copyright/license headers to zeitgeist/engine/*, this makes distributions happy :). (Please check I haven't forgot anybody. Others .py files should also get the same headers, btw.).

--- zeitgeist/engine/base.py
+++ zeitgeist/engine/base.py
@@ -1,8 +1,22 @@
-'''
-Created on Jun 6, 2009
+# -.- encoding: utf-8 -.-
 
-@author: seif, kamstrup
-'''
+# Zeitgeist
+#
+# Copyright © 2009 Seif Lotfy <seif@lotfy.com>
+# Copyright © 2009 Mikkel Kamstrup Erlandsen <mikkel.kamstrup@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 from storm.locals import *
