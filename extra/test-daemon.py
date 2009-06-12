4b9bd533208e06ecbde139c8e8861c68fc159670
Siegfried-Angel Gevatter Pujals <rainct@ubuntu.com>
2009-06-12 17:29:16 +0200

    Change D-Bus methods's names to CamelCase (LP: #383342).

--- extra/test-daemon.py
+++ extra/test-daemon.py
@@ -20,26 +20,26 @@ if '--listen' in sys.argv:
 iface = dbus.Interface(remote_object, "org.gnome.Zeitgeist")
 
 print '\nDifferent types in the database:'
-for name, icon in iface.get_types():
+for name, icon in iface.GetTypes():
 	print '- %s (%s)' % (name, icon)
 print
 
 print 'Your bookmarks are:'
-bookmarks = iface.get_bookmarks()
+bookmarks = iface.GetBookmarks()
 first_item = None
 for bookmark in bookmarks:
 	print '-', bookmark[2], '«' +  urllib.unquote(str(bookmark[1])) + '»'
 	if not first_item: first_item = bookmark
 
 print '\nYour most used tags are:'
-print '-', ', '.join(iface.get_most_used_tags(0, 0, 0))
+print '-', ', '.join(iface.GetMostUsedTags(0, 0, 0))
 
 print '\nYour recently used tags are:'
-print '-', ', '.join(iface.get_recent_used_tags(0, 0, 0))
+print '-', ', '.join(iface.GetRecentUsedTags(0, 0, 0))
 
 mimetype = 'text/plain'
 print '\nItems with mimetype «%s»:' % mimetype
-for item in iface.get_items_with_mimetype(mimetype, 0, 0, '')[:5]:
+for item in iface.GetItemsWithMimetype(mimetype, 0, 0, '')[:5]:
 	print '-', item[2]
 
 if first_item:
@@ -47,13 +47,13 @@ if first_item:
 	print '-', ', '.join(first_item[4].split(','))
 	
 	print u'\nItems related to «%s»:' % first_item[2]
-	related_items = iface.get_related_items(first_item[1])
+	related_items = iface.GetRelatedItems(first_item[1])
 	for related_item in related_items[:5]:
 		print '-', related_item[2]
 	del related_items
 	
 	print u'\nItems sharing some tag with «%s»:' % first_item[2]
-	related_items = iface.get_items_related_by_tags(first_item[1])
+	related_items = iface.GetItemsRelatedByTags(first_item[1])
 	for related_item in related_items[:5]:
 		print '-', related_item[2] + ':', ', '.join(related_item[6].split(','))
 	del related_items
@@ -61,7 +61,7 @@ if first_item:
 	last_tag = first_item[4].split(',')[-1]
 	if last_tag:
 		print u'\nItems with tag «%s»:' % last_tag
-		tag_items = iface.get_items(0, 0, last_tag, '')
+		tag_items = iface.GetItems(0, 0, last_tag, '')
 		for tag_item in tag_items[:5]:
 			print '-', tag_item[2] + ':', ', '.join(tag_item[6].split(','))
 
