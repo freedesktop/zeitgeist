62b6a1b9f0e8690ec3f655573a186deaecebe8b7
Siegfried-Angel Gevatter Pujals <rainct@ubuntu.com>
2009-06-13 19:29:46 +0200

    remove salt as it's unused now

--- zeitgeist/engine/engine.py
+++ zeitgeist/engine/engine.py
@@ -38,7 +38,6 @@ from zeitgeist.shared.zeitgeist_shared import *
 from zeitgeist.engine.base import *
 
 class ZeitgeistEngine(gobject.GObject):
-	_salt = 0
 
 	def __init__(self, storm_store):
 		
@@ -101,10 +100,6 @@ class ZeitgeistEngine(gobject.GObject):
 			return item["uri"]
 		
 		return item
-	
-	def _next_salt(self):
-		self._salt += 1
-		return self._salt
 	
 	def get_last_timestamp(self, uri=None):
 		"""
