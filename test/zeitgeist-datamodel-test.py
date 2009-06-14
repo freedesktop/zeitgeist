e9e487378d24a8cc8541c050a8b9d95a0f5c99ae
Mikkel Kamstrup Erlandsen <mikkel.kamstrup@gmail.com>
2009-06-14 20:09:51 +0200

    Full test suite for Content and Source is passing

--- test/zeitgeist-datamodel-test.py
+++ test/zeitgeist-datamodel-test.py
@@ -20,9 +20,61 @@ class CategoryTest (unittest.TestCase):
 		pass
 	
 	def testAbstractClass(self):
+		"""Make sure that we can not instantiate a Cateory directly"""
 		self.failUnlessRaises(ValueError, Category, "http://example.com/schema#Foo")
 		self.failUnlessRaises(ValueError, Category.get, "http://example.com/schema#Foo")
-			
+
+class ContentTest (unittest.TestCase):
+	"""
+	This class tests that the zeitgeist.datamodel.Content class
+	"""
+	def setUp (self):
+		pass
+		
+	def tearDown (self):
+		Content.CACHE = {}
 	
+	def testCleanCache(self):
+		"""Assert that the cache is clear - for the sake of a clean test env."""
+		self.assertEquals(0, len(Content.CACHE))
+
+	def testConstructors(self):
+		foo_url = "http://example.com/schema#Foo"
+		foo = Content(foo_url)
+		self.assertEquals("Foo", foo.name)
+		self.assertEquals(foo_url, foo.uri)
+	
+	def testCache(self):
+		foo_url = "http://example.com/schema#Foo"
+		foo1 = Content(foo_url)
+		foo2 = Content.get(foo_url)
+		self.assertEquals(id(foo1), id(foo2))
+
+class SourceTest (unittest.TestCase):
+	"""
+	This class tests that the zeitgeist.datamodel.Source class
+	"""
+	def setUp (self):
+		pass
+		
+	def tearDown (self):
+		Source.CACHE = {}
+
+	def testCleanCache(self):
+		"""Assert that the cache is clear - for the sake of a clean test env."""
+		self.assertEquals(0, len(Source.CACHE))
+
+	def testConstructors(self):
+		foo_url = "http://example.com/schema#Foo"
+		foo = Source(foo_url)
+		self.assertEquals("Foo", foo.name)
+		self.assertEquals(foo_url, foo.uri)
+	
+	def testCache(self):
+		foo_url = "http://example.com/schema#Foo"
+		foo1 = Source(foo_url)
+		foo2 = Source.get(foo_url)
+		self.assertEquals(id(foo1), id(foo2))
+
 if __name__ == '__main__':
 	unittest.main()
--- zeitgeist/datamodel.py
+++ zeitgeist/datamodel.py
@@ -1,30 +1,52 @@
+def _(s) : return s
+
 class Category:
 	CACHE = {}
 	
-	def __init__ (self, url, display_name=None, name=None):
+	def __init__ (self, uri, display_name=None, name=None):
 		if self.__class__ == Category:
 			raise ValueError("Category is an abstract class")
-		self.url = url
+		self.uri = uri
 		
 		if name : self.name = name
-		else: self.name = url.split("#")[1]
+		else: self.name = uri.split("#")[1]
 		
 		if display_name : self.display_name = display_name
 		else: self.display_name = self.name
 		
-		self.__class__.CACHE[url] = self
+		self.__class__.CACHE[uri] = self
 
 	@classmethod
-	def get(klass, url):
-		if not url in klass.CACHE:
-			this = klass(url)
-			klass.CACHE[url] = this
-		return klass.CACHE[url]
+	def get(klass, uri):
+		if not uri in klass.CACHE:
+			this = klass(uri)
+			klass.CACHE[uri] = this
+		return klass.CACHE[uri]
 
 class Content(Category):
 	def __init__ (self, url, display_name=None, name=None):
-		super(Content, self).__init__(url, displayName, name)
+		Category.__init__(self, url, display_name, name)
 
 class Source(Category):
 	def __init__ (self, url, display_name=None, name=None):
-		super(Source, self).__init__(url, displayName, name)
+		Category.__init__(self, url, display_name, name)
+
+Content.TAG = Content("http://freedesktop.org/standards/xesam/1.0/core#Tag",
+					  display_name=_("Tags"))
+Content.BOOKMARK = Content("http://freedesktop.org/standards/xesam/1.0/core#Bookmark",
+						   display_name=_("Bookmarks"))
+"""Content.COMMENT = Symbol(Content, "http://gnome.org/zeitgeist/schema/1.0/core#Comment")
+Content.DOCUMENT = Symbol(Content, "http://freedesktop.org/standards/xesam/1.0/core#Document")
+Content.CREATE_EVENT = Symbol(Content, "http://gnome.org/zeitgeist/schema/1.0/core#CreateEvent")
+Content.MODIFY_EVENT = Symbol(Content, "http://gnome.org/zeitgeist/schema/1.0/core#ModifyEvent")
+Content.VISIT_EVENT = Symbol(Content, "http://gnome.org/zeitgeist/schema/1.0/core#VisitEvent")
+Content.LINK_EVENT = Symbol(Content, "http://gnome.org/zeitgeist/schema/1.0/core#LinkEvent")
+Content.SEND_EVENT = Symbol(Content, "http://gnome.org/zeitgeist/schema/1.0/core#SendEvent")
+Content.RECEIVE_EVENT = Symbol(Content, "http://gnome.org/zeitgeist/schema/1.0/core#ReceiveEvent")
+Content.WARN_EVENT = Symbol(Content, "http://gnome.org/zeitgeist/schema/1.0/core#WarnEvent")
+Content.ERROR_EVENT = Symbol(Content, "http://gnome.org/zeitgeist/schema/1.0/core#ErrorEvent")
+Source.WEB_HISTORY = Symbol(Source, "http://gnome.org/zeitgeist/schema/1.0/core#WebHistory")
+Source.USER_ACTIVITY = Symbol(Source, "http://gnome.org/zeitgeist/schema/1.0/core#UserActivity")
+Source.USER_NOTIFICATION = Symbol(Source, "http://gnome.org/zeitgeist/schema/1.0/core#UserNotification")
+Source.APPLICATION = Symbol(Source, "http://gnome.org/zeitgeist/schema/1.0/core#Application")
+"""
