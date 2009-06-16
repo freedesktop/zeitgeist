a4365f8efbeb0e38c6692c7b69c09a90cb3edd85
Markus Korn <thekorn@gmx.de>
2009-06-16 17:23:37 +0200

    * zeitgeist/loggers/datasources/zeitgeist_recent.py:   using gtk.RecentManager to get recently changed files

--- zeitgeist/loggers/datasources/zeitgeist_recent.py
+++ zeitgeist/loggers/datasources/zeitgeist_recent.py
@@ -25,10 +25,9 @@ import os
 import re
 import fnmatch
 import urllib
-import time
 import gtk
 import gettext
-import glob
+import time
 
 from xdg import DesktopEntry, BaseDirectory
 
@@ -38,37 +37,6 @@ from zeitgeist import config
 gettext.install("zeitgeist", config.localedir, unicode=1)
 		
 # helpers
-
-# workaround pygtk bug
-def find_desktopfiles():
-	for dir in BaseDirectory.load_data_paths("applications"):
-		for desktopfile in glob.glob("%s/*.desktop" % dir):
-			yield desktopfile
-
-ADDITIONS = (
-	(u"Text Editor", "/usr/share/applications/gedit.desktop"),
-	(u"File Roller", "/usr/share/applications/file-roller.desktop"),
-	(u"Document Viewer", "/usr/share/applications/evince.desktop"),
-	(u"Eye of GNOME Image Viewer", "/usr/share/applications/eog.desktop"),
-	(u"GNU Image Manipulation Program", "/usr/share/applications/gimp.desktop"),
-	(u"Totem Video-Player", "/usr/share/applications/totem.desktop"),
-	(u"File Manager", "/usr/share/applications/nautilus.desktop"),
-	(u"Anjuta", "/usr/share/applications/anjuta.desktop")
-)
-
-def create_app_map():
-	for desktopfile in find_desktopfiles():
-		entry = DesktopEntry.DesktopEntry(desktopfile)
-		# add entry for name attribute
-		yield (unicode(entry.getName()), (desktopfile, entry))
-		# add entry for executable
-		if entry.getExec():
-			yield (unicode(entry.getExec().split()[0].split("/")[-1]),
-				(desktopfile, entry))
-	for identifier, desktopfile in ADDITIONS:
-		yield (unicode(identifier), (desktopfile, DesktopEntry.DesktopEntry(desktopfile)))
-
-DESKTOPFILE_MAP = dict(create_app_map())
 
 def get_desktopentry_for_application(application):
 	desktopfiles = list(
@@ -84,7 +52,7 @@ def get_desktopentry_for_application(application):
 		# raise an error? or try to get an alternative file?
 		# Example gimp-s.6 has no .desktop file
 		return get_desktopentry_for_application("firefox") #just for now, for testing
-
+			
 
 class SimpleMatch(object):
 
@@ -122,7 +90,7 @@ class MimeTypeSet(set):
 	def __repr__(self):
 		items = ", ".join(map(repr, self | self.__pattern))
 		return "%s(%s)" %(self.__class__.__name__, items)
-
+		
 
 DOCUMENT_MIMETYPES = [
 		# Covers:
@@ -212,13 +180,6 @@ DEVELOPMENT_MIMETYPES = [
 		]
 		
 
-BROKEN_APPS = [
-			   u"Document Viewer",
-			   u"Text Editor",
-			   u"Totem Movie Player",
-			   u"File Manager"
-			   ]
-
 class RecentlyUsedManagerGtk(DataProvider):
 	
 	def __init__(self):
@@ -226,8 +187,10 @@ class RecentlyUsedManagerGtk(DataProvider):
 		self.recent_manager = gtk.recent_manager_get_default()
 		self.recent_manager.set_limit(-1)
 		self.recent_manager.connect("changed", lambda m: self.emit("reload"))
+		self._timestamp_last_run = 0
 		
 	def get_items_uncached(self):
+		timestamp_last_run = time.time()
 		for info in self.recent_manager.get_items():
 			if info.exists() and not info.get_private_hint() and "/tmp" not in info.get_uri_display():
 				use = None
@@ -245,46 +208,37 @@ class RecentlyUsedManagerGtk(DataProvider):
 					tmp = unicode(urllib.unquote(tmp))
 					tags = tmp.replace(",", " ").replace("/", ",")
 				
-				uri = unicode(info.get_uri_display())
+				uri = unicode(info.get_uri())
 				text = info.get_display_name()
 				mimetype = unicode(info.get_mime_type())
 				last_application = info.last_application().strip()
+				application_info = info.get_application_info(last_application)
+				application = application_info[0].split()[0]
+				desktopfile, desktopentry = get_desktopentry_for_application(application)
+				icon = desktopentry.getIcon()
+				origin = None # we are not sure about the origin of this item, let's make it NULL
+				times = (
+					(info.get_added(), u"CreateEvent"),
+					(info.get_visited(), u"VisitEvent"),
+					(info.get_modified(), u"ModifyEvent")
+				)
 				
-				# this causes a  *** glibc detected *** python: double free or corruption (!prev): 0x0000000001614850 ***
-				# bug in pygtk, reported as (lp: #386035) and upstream
-				#application_info = info.get_application_info(last_application)
-				#application = application_info[0].split()[0]
-				#desktopfile, desktopentry = get_desktopentry_for_application(application)
-				if not "totem" in last_application.lower():
-					try:
-						desktopfile, desktopentry = DESKTOPFILE_MAP[last_application]
-					except KeyError:
-						print ">>> please add an addition for '%s' to DESKTOPFILE_MAP" %last_application
-						raise
-					# ^^ please change the code above once the fix for the pygtk bug landed
-					icon = desktopentry.getIcon()
-					origin = u"%s:///" %info.get_uri().split(":///")[0]
-					times = (
-						(info.get_added(), u"CreateEvent"),
-						(info.get_visited(), u"VisitEvent"),
-						(info.get_modified(), u"ModifyEvent")
-					)
-					for timestamp, use in times:
-						item = {
-							"timestamp": timestamp,
-							"uri": uri if uri[uri.find('/')-1:uri.find('/')+2] == '://' \
-								else unicode(origin[:-1] + uri),
-							"text": text,
-							#~ "source": u"File",
-							"content": u"File",
-							"use": u"http://gnome.org/zeitgeist/schema/1.0/core#%s" %use,
-							"mimetype": mimetype,
-							"tags": tags,
-							"icon": icon,
-							"app": unicode(desktopfile),
-							"origin": origin,
-						}
-						yield item
+				for timestamp, use in times:
+					item = {
+						"timestamp": timestamp,
+						"uri": uri,
+						"text": text,
+						#~ "source": u"File",
+						"content": u"File",
+						"use": u"http://gnome.org/zeitgeist/schema/1.0/core#%s" %use,
+						"mimetype": mimetype,
+						"tags": tags,
+						"icon": icon,
+						"app": unicode(desktopfile),
+					}
+					yield item
+		self._timestamp_last_run = timestamp_last_run
+
 
 class RecentlyUsed(DataProvider):
 	"""
@@ -293,7 +247,6 @@ class RecentlyUsed(DataProvider):
 	def __init__(self, name, icon="stock_calendar"):
 		DataProvider.__init__(self, name=name, icon=icon)
 		recent_model.connect("reload", lambda m: self.emit("reload"))
-		self.timestamp_flag = 0
 		self.counter = 0
 		self.last_uri = None
 	
@@ -301,8 +254,9 @@ class RecentlyUsed(DataProvider):
 		self.counter = self.counter + 1
 		if recent_model:
 			for item in recent_model.get_items_uncached():
-				if self.include_item(item):
-					yield item
+				if item["timestamp"] >= recent_model._timestamp_last_run:
+					if self.include_item(item):
+						yield item
 				
 	def include_item(self, item):
 		return True
@@ -324,12 +278,11 @@ class RecentlyUsedOfMimeType(RecentlyUsed):
 		return item["mimetype"] in self.mimetype_list
 	
 	def get_items_uncached(self):
+		print "RecentlyUsedOfMimeType"
 		for item in RecentlyUsed.get_items_uncached(self):
-			if item["timestamp"] >= self.timestamp_flag:
-				item["icon"] = self.icon
-				item["source"] = unicode(self.filter_name)
-				yield item
-		self.timestamp_flag = time.time()
+			item["icon"] = self.icon
+			item["source"] = unicode(self.filter_name)
+			yield item
 
 
 class RecentlyUsedDocumentsSource(RecentlyUsedOfMimeType):
