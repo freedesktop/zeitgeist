eb2db39b3a53c1f37ee807681dfebe6909f40e7f
Natan Yellin <aantny@gmail.com>
2009-06-21 11:53:08 +0300

    Add a new script to run all of the unit tests at once.
    Tests must be in test/ and end with -test.py in order to be run. (The benchmark test was excluded on purpose.) Please note that there's currently something wrong with the logger test. It fails when it is run alone and stops failing when it is run as part of the test suite.

---
+++ test/zeitgeist-run-all-tests.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import os
+import unittest
+
+# Create a test suite to run all tests
+suite = unittest.TestSuite()
+
+# Find the test/ directory
+testdir = os.path.dirname(os.path.abspath(__file__))
+
+# Add all of the tests from each file that ends with "-test.py"
+for fname in os.listdir(testdir):
+	if fname.endswith("-test.py") and not fname.endswith("zeitgeist-run-all-tests.py"):
+		fname = os.path.basename(fname)[:-3] # Get the filename and chop off ".py"
+		module = __import__(fname)
+		suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(module))
+
+# Run all of the tests
+unittest.TextTestRunner(verbosity=2).run(suite)
