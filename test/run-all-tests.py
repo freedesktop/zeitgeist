6eef6f350cba1364f718a92a482350f2a2a8a063
Siegfried-Angel Gevatter Pujals <rainct@ubuntu.com>
2009-06-24 17:36:30 +0200

    Rename files in test/ to remove the zeitgeist- prefix from their name.

--- test/run-all-tests.py
+++ test/run-all-tests.py
@@ -11,7 +11,7 @@ testdir = os.path.dirname(os.path.abspath(__file__))
 
 # Add all of the tests from each file that ends with "-test.py"
 for fname in os.listdir(testdir):
-	if fname.endswith("-test.py") and not fname.endswith("zeitgeist-run-all-tests.py"):
+	if fname.endswith("-test.py") and not fname.endswith("run-all-tests.py"):
 		fname = os.path.basename(fname)[:-3] # Get the filename and chop off ".py"
 		module = __import__(fname)
 		suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(module))
