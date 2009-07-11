904564990fe167d19108c44a9ab6eed7a0d3d87a
Markus Korn <thekorn@gmx.de>
2009-07-11 20:46:19 +0200

    * fixed imports, we don't need gettext and sys in dbusutils anymore

--- zeitgeist/dbusutils.py
+++ zeitgeist/dbusutils.py
@@ -19,10 +19,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
 import dbus
 import dbus.mainloop.glib
-import gettext
 import logging
 
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
