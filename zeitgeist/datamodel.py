4d7474d8c204eff0015289abbc091bd451299599
Markus Korn <thekorn@gmx.de>
2009-08-03 20:56:18 +0200

    added license header

--- zeitgeist/datamodel.py
+++ zeitgeist/datamodel.py
@@ -1,3 +1,30 @@
+# -.- coding: utf-8 -.-
+
+# Zeitgeist
+#
+# Copyright © 2009 Mikkel Kamstrup Erlandsen <mikkel.kamstrup@gmail.com>
+# Copyright © 2009 Markus Korn <thekorn@gmx.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+This module provides the abstract datamodel used by the Zeitgeist framework.
+In addition to providing useful constructs for dealing with the Zeitgeist data
+it also defines symbolic values for the common item types. Using symbolic values
+instead of Uri string will help detect programmer typos.
+"""
+
 import gettext
 gettext.install("zeitgeist")
 
