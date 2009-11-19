ab8733e7190d66c90f4d9149381851f79f8a96b7
Mikkel Kamstrup Erlandsen <mikkel.kamstrup@gmail.com>
2009-11-19 14:35:23 +0100

    Move useful functionality from ZeitgeistRemoteAPITest in remote-test.py into a new convenience module called 'testutils'. Also move json_importer into testutils.py and change resonance-engine-test.py to use this module instead of json_importer.py

--- test/remote-test.py
+++ test/remote-test.py
@@ -16,95 +16,12 @@ DBusGMainLoop(set_as_default=True)
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 from zeitgeist.dbusutils import DBusInterface, ZeitgeistClient
 from zeitgeist.datamodel import Event, Subject, Interpretation, Manifestation, TimeRange
+import testutils
 
-class ZeitgeistRemoteAPITest(unittest.TestCase):
+class ZeitgeistRemoteAPITest(testutils.RemoteTestCase):
 	
 	def __init__(self, methodName):
 		super(ZeitgeistRemoteAPITest, self).__init__(methodName)
-		self.daemon = None
-		self.client = None
-	
-	def spawn_daemon(self):
-		os.environ.update({"ZEITGEIST_DATABASE_PATH": ":memory:"})
-		self.daemon = Popen(["./zeitgeist-daemon", "--no-passive-loggers"])
-		# give the daemon some time to wake up
-		time.sleep(3)
-		err = self.daemon.poll()
-		if err:
-			raise RuntimeError("Could not start daemon,  got err=%i" %err)
-		
-		
-	def kill_daemon(self):
-		os.kill(self.daemon.pid, signal.SIGKILL)
-		self.daemon.wait()
-		
-	def setUp(self):
-		assert self.daemon is None
-		assert self.client is None
-		self.spawn_daemon()
-		
-		# hack to clear the state of the interface
-		DBusInterface._DBusInterface__shared_state = {}
-		self.client = ZeitgeistClient()
-		
-	def tearDown(self):
-		assert self.daemon is not None
-		assert self.client is not None
-		self.kill_daemon()
-	
-	def insertEventsAndSpin(self, events):
-		"""
-		Insert a set of events and spin a mainloop until the async reply
-		is back and return the result - which should be a list of ids.
-		"""
-		mainloop = gobject.MainLoop()
-		result = []
-		
-		def collect_ids_and_quit(ids):
-			result.extend(ids)
-			mainloop.quit()
-			
-		self.client.insert_events(events,
-					ids_reply_handler=collect_ids_and_quit)
-		mainloop.run()
-		return result
-	
-	def findEventIdsAndSpin(self, timerange, event_templates, **kwargs):
-		"""
-		Do search based on event_templates and spin a mainloop until
-		the async reply is back and return the result - which should be
-		a list of ids.
-		"""
-		mainloop = gobject.MainLoop()
-		result = []
-		
-		def collect_ids_and_quit(ids):
-			result.extend(ids)
-			mainloop.quit()
-			
-		self.client.find_event_ids_for_templates(collect_ids_and_quit,
-							timerange,
-							event_templates,
-							**kwargs)
-		mainloop.run()
-		return result
-	
-	def getEventsAndSpin(self, event_ids):
-		"""
-		Request a set of full events and spin a mainloop until the
-		async reply is back and return the result - which should be a
-		list of Events
-		"""
-		mainloop = gobject.MainLoop()
-		result = []
-		
-		def collect_events_and_quit(events):
-			result.extend(events)
-			mainloop.quit()
-			
-		self.client.get_events(collect_events_and_quit, event_ids)
-		mainloop.run()
-		return result
 	
 	def testInsertAndGetEvent(self):
 		ev = Event.new_for_values(timestamp=123,
@@ -115,8 +32,8 @@ class ZeitgeistRemoteAPITest(unittest.TestCase):
 					interpretation=Interpretation.DOCUMENT.uri,
 					manifestation=Manifestation.FILE.uri)
 		ev.append_subject(subj)
-		ids = self.insertEventsAndSpin([ev])
-		events = self.getEventsAndSpin(ids)
+		ids = self.insertEventsAndWait([ev])
+		events = self.getEventsAndWait(ids)
 		self.assertEquals(1, len(ids))
 		self.assertEquals(1, len(events))
 		
@@ -158,10 +75,10 @@ class ZeitgeistRemoteAPITest(unittest.TestCase):
 		ev2.append_subject(subj2)
 		ev3.append_subject(subj2)
 		ev3.append_subject(subj3)
-		ids = self.insertEventsAndSpin([ev1, ev2, ev3])
+		ids = self.insertEventsAndWait([ev1, ev2, ev3])
 		self.assertEquals(3, len(ids))
 		
-		events = self.getEventsAndSpin(ids)
+		events = self.getEventsAndWait(ids)
 		self.assertEquals(3, len(events))		
 		for event in events:
 			self.assertTrue(isinstance(event, Event))
@@ -170,7 +87,7 @@ class ZeitgeistRemoteAPITest(unittest.TestCase):
 		
 		# Search for everything
 		import dbus
-		ids = self.findEventIdsAndSpin(TimeRange(0,10000),
+		ids = self.findEventIdsAndWait(TimeRange(0,10000),
 						[], num_events=3) # dbus.Array(signature="(asaasay)")
 		self.assertEquals(3, len(ids)) # (we can not trust the ids because we don't have a clean test environment)
 		
@@ -181,7 +98,7 @@ class ZeitgeistRemoteAPITest(unittest.TestCase):
 					actor="Boogaloo",
 					interpretation=Interpretation.VISIT_EVENT.uri,
 					subjects=[subj_templ1,subj_templ2])
-		ids = self.findEventIdsAndSpin(TimeRange(0,10000),
+		ids = self.findEventIdsAndWait(TimeRange(0,10000),
 						[event_template],
 						num_events=10)
 		print "RESULTS", map(int, ids)
