1c799ad3b062c942b889a21ca671fceb6c1ddfff
Markus Korn <thekorn@gmx.de>
2009-12-10 13:31:35 +0100

    added failing testcase to test insert_hook for extensions

--- test/resonance-engine-extension-test.py
+++ test/resonance-engine-extension-test.py
@@ -6,10 +6,13 @@ import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
+import _zeitgeist
 from _zeitgeist.engine.resonance_engine import ZeitgeistEngine
 from _zeitgeist.engine.extension import Extension
+from _zeitgeist.engine import create_engine
 
 import unittest
+from testutils import import_events
 
 class _Extension1(Extension):
 	PUBLIC_METHODS = ["return_hallo", "return_engine"]
@@ -22,6 +25,17 @@ class _Extension1(Extension):
 		
 	def return_engine(self):
 		return self.engine
+
+
+class _engineTestClass(unittest.TestCase):
+	
+	def setUp (self):
+		_zeitgeist.engine.DB_PATH = ":memory:"
+		self.engine = create_engine()
+		
+	def tearDown (self):
+		self.engine.close()
+		_zeitgeist.engine._engine = None
 	
 
 class TestExtensions(unittest.TestCase):
@@ -34,6 +48,23 @@ class TestExtensions(unittest.TestCase):
 		self.assertEqual(engine.extensions.return_hallo(), "Hallo")
 		self.assertRaises(AttributeError, engine.extensions.__getattr__, "return_boo")
 		self.assertEqual(engine.extensions.return_engine(), engine)
+		
+		
+class TestExtensionHooks(_engineTestClass):
+	
+	def testInsertHook(self):
+		
+		class BlockAllInsertExtension(Extension):
+			PUBLIC_METHODS = ["return_hallo",]
+			
+			def return_hallo(self):
+				return "HALLO"
+				
+			def insert_event_hook(self, event):
+				return None
+				
+		self.engine.extensions.load(BlockAllInsertExtension)
+		ids = import_events("test/data/five_events.js", self.engine)
 
 if __name__ == "__main__":
 	unittest.main()
