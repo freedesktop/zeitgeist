0d48555cc7df25c338a208be5268eac5de77770f
Siegfried-Angel Gevatter Pujals <rainct@ubuntu.com>
2009-12-30 21:35:32 +0100

    Add Bash tab-completion.

--- zeitgeist-daemon.py
+++ zeitgeist-daemon.py
@@ -25,7 +25,7 @@ import subprocess
 import dbus.mainloop.glib
 import gettext
 import logging
-from optparse import OptionParser
+import optparse
 
 from zeitgeist import _config
 _config.setup_path()
@@ -33,21 +33,32 @@ _config.setup_path()
 gettext.install("zeitgeist", _config.localedir, unicode=1)
 logging.basicConfig(level=logging.DEBUG)
 
-parser = OptionParser(version = _config.VERSION)
+parser = optparse.OptionParser(version = _config.VERSION)
 parser.add_option(
-	'-r', '--replace',
-	action = 'store_true', default=False, dest = 'replace',
-	help = _('if another Zeitgeist instance is already running, replace it'))
+	"-r", "--replace",
+	action = "store_true", default=False, dest = "replace",
+	help = _("if another Zeitgeist instance is already running, replace it"))
 parser.add_option(
-	'--no-passive-loggers',
-	action = 'store_false', default=True, dest = 'start_datahub',
-	help = _('do not start zeitgeist-datahub automatically'))
+	"--no-passive-loggers",
+	action = "store_false", default=True, dest = "start_datahub",
+	help = _("do not start zeitgeist-datahub automatically"))
 parser.add_option(
-	'--quit',
-	action = 'store_true', default=False, dest = 'quit',
-	help = _('if another Zeitgeist instance is already running, replace it'))
+	"--quit",
+	action = "store_true", default=False, dest = "quit",
+	help = _("if another Zeitgeist instance is already running, replace it"))
+parser.add_option(
+	"--shell-completion",
+	action = "store_true", default=False, dest = "shell_completion",
+	help = optparse.SUPPRESS_HELP)
 
 (_config.options, _config.arguments) = parser.parse_args()
+
+if _config.options.shell_completion:
+	options = set()
+	for option in (str(option) for option in parser.option_list):
+		options.update(option.split("/"))
+	print ' '.join(options)
+	sys.exit(0)
 
 from _zeitgeist.engine.remote import RemoteInterface
 
