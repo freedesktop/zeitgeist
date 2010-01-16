ecc5cabe9a3349ee08d9fce5cc2a0aecd0727fe5
Siegfried-Angel Gevatter Pujals <rainct@ubuntu.com>
2010-01-16 19:21:11 +0100

    Gah.. Copyright/Licensing fixes.

--- _zeitgeist/engine/main.py
+++ _zeitgeist/engine/main.py
@@ -6,7 +6,6 @@
 # Copyright © 2009-2010 Siegfried-Angel Gevatter Pujals <rainct@ubuntu.com>
 # Copyright © 2009 Seif Lotfy <seif@lotfy.com>
 # Copyright © 2009 Markus Korn <thekorn@gmx.net>
-# Copyright © 2009 Alexander Gabriel <einalex@mayanna.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
