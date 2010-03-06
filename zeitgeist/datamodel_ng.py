cb63db3a4728311387bad7510cfb606b210775db
Markus Korn <thekorn@gmx.de>
2010-03-06 07:32:38 +0100

    * fixed empty spaces at line ending * added header to zeitgeist/datamodel_ng.py

--- zeitgeist/datamodel_ng.py
+++ zeitgeist/datamodel_ng.py
@@ -1,3 +1,22 @@
+# -.- coding: utf-8 -.-
+
+# Zeitgeist
+#
+# Copyright © 2009 Markus Korn <thekorn@gmx.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os.path
 import gettext
 gettext.install("zeitgeist", unicode=1)
@@ -15,8 +34,8 @@ else:
 	# ontology. This is why we parse the ontology to a temporary file
 	# and load it from there
 	IS_LOCAL = True
-	
-	
+
+
 class EnumValue(int):
 	"""class which behaves like an int, but has an additional docstring"""
 	def __new__(cls, value, doc=""):
@@ -26,31 +45,31 @@ class EnumValue(int):
 
 
 class Enum(object):
-	
+
 	def __init__(self, docstring):
 		self.__doc__ = str(docstring)
 		self.__enums = {}
-		
+
 	def __getattr__(self, name):
 		try:
 			return self.__enums[name]
 		except KeyError:
 			raise AttributeError
-			
+
 	def register(self, value, name, docstring):
 		ids = map(int, self.__enums.values())
 		if value in ids or name in self.__enums:
 			raise ValueError
 		self.__enums[name] = EnumValue(value, docstring)
 
-	
+
 class Symbol(str):
-	
+
 	"""Immutable string-like object representing a Symbol
-	Zeitgeist uses Symbols when defining Manifestations and 
+	Zeitgeist uses Symbols when defining Manifestations and
 	Interpretations.
 	"""
-	
+
 	def __new__(cls, symbol_type, name, uri=None, display_name=None, doc=None):
 		obj = super(Symbol, cls).__new__(Symbol, uri or name)
 		obj.__type = symbol_type
@@ -59,38 +78,38 @@ class Symbol(str):
 		obj.__display_name = display_name
 		obj.__doc = doc
 		return obj
-	
+
 	def __repr__(self):
 		return "<%s %r>" %(self.__type, self.uri)
-	
+
 	@property
 	def uri(self):
 		return self.__uri or self.name
-	
+
 	@property
 	def display_name(self):
 		return self.__display_name or ""
-	
+
 	@property
 	def name(self):
 		return self.__name
-	
+
 	@property
 	def doc(self):
 		return self.__doc or ""
-		
+
 	@property
 	def __doc__(self):
 		return "%s\n\n	%s. ``(Display name: '%s')``" %(self.uri, self.doc.rstrip("."), self.display_name)
 
 
 class SymbolCollection(object):
-	
+
 	def __init__(self, name, doc=""):
 		self.__name__ = name
 		self.__doc__ = str(doc)
 		self.__symbols = {}
-	
+
 	def register(self, name, uri, display_name, doc):
 		if name in self.__symbols:
 			raise ValueError("Cannot register symbol %r, a definition for "
@@ -98,10 +117,10 @@ class SymbolCollection(object):
 		if not name.isupper():
 			raise ValueError("Cannot register %r, name must be uppercase" %name)
 		self.__symbols[name] = Symbol(self.__name__, name, uri, display_name, doc)
-	
+
 	def __len__(self):
 		return len(self.__symbols)
-	
+
 	def __getattr__(self, name):
 		if not name in self.__symbols:
 			if not name.isupper():
@@ -111,27 +130,27 @@ class SymbolCollection(object):
 			print "Unrecognized %s: %s" % (self.__name__, name)
 			self.__symbols[name] = Symbol(self.__name__, name)
 		return self.__symbols[name]
-	
+
 	def __getitem__(self, uri):
 		""" Get a symbol by its URI. """
 		symbol = [s for s in self.__symbols.values() if s.uri == uri]
 		if symbol:
 			return symbol[0]
 		raise KeyError("Could not find symbol for URI: %s" % uri)
-	
+
 	def __iter__(self):
 		return self.__symbols.itervalues()
-	
+
 	def __dir__(self):
 		return self.__symbols.keys()
 
 
 def register_symbol(collection, name, uri, displayname, docstring):
 	collection.register(name, uri, displayname, docstring)
-	
+
 def register_enum(enum, value, name, docstring):
 	enum.register(value, name, docstring)
-	
+
 if IS_LOCAL:
 	from tempfile import NamedTemporaryFile
 	from subprocess import Popen, PIPE
@@ -149,14 +168,14 @@ if IS_LOCAL:
 	fd.close()
 else:
 	raise NotImplementedError
-	# it should be similar to 
+	# it should be similar to
 	execfile("/path/of/zeo.trig.py")
 
 if __name__ == "__main__":
 	# testing
 	print dir(EventManifestation)
 	print EventManifestation.USER_ACTIVITY
-	
+
 	print StorageState
 	print StorageState.Any
 	print StorageState.Any.__doc__
