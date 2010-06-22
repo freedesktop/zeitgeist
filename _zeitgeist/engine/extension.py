c9aae9fd2cea4eaa58702f80538e5a3a5a524bee
Mikkel Kamstrup Erlandsen <kamstrup@hardback>
2010-06-22 21:54:16 +0200

    Update copyrights for extension.py

--- _zeitgeist/engine/extension.py
+++ _zeitgeist/engine/extension.py
@@ -3,6 +3,7 @@
 # Zeitgeist
 #
 # Copyright © 2009 Markus Korn <thekorn@gmx.de>
+# Copyright © 2010 Mikkel Kamstrup Erlandsen <mikkel.kamstrup@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
