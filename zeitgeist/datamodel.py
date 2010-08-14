992df92bb7ea56de0a65c5eafa75ba6ec9ac22d8
Seif Lotfy <seif@lotfy.com>
2010-08-15 01:38:39 +0200

    fix the issue raised by mhr3 with the return of is_always

--- zeitgeist/datamodel.py
+++ zeitgeist/datamodel.py
@@ -384,7 +384,7 @@ class TimeRange(list):
 		Returns True if this time range represents the 0 (January 1, 1970) to the most
 		distant future
 		"""
-		return self.begin <= -TimeRange._max_stamp and self.end >= TimeRange._max_stamp
+		return self.begin == 0 and self.end >= TimeRange._max_stamp
 		
 	def intersect(self, time_range):
 		"""
