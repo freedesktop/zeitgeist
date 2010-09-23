c12d4a6b1c2196cece85c894d83f2aa6a309bfd5
Markus Korn <thekorn@gmx.de>
2010-09-23 13:02:41 +0200

    added a test script for seif's issue

---
+++ bug_testrunner.py
@@ -0,0 +1,9 @@
+import os
+import glob
+from subprocess import Popen
+
+tests = map(lambda x: os.path.basename(x).rsplit(".", 1)[0], glob.glob("test/*.rst"))
+tests += map(lambda x: os.path.basename(x).rsplit(".", 1)[0], glob.glob("test/*-test.py"))
+
+for test in tests:
+    Popen(["test/run-all-tests.py", test, "blacklist-test"]).wait()
