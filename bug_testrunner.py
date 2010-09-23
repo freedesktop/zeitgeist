c0c749769ccc83b18e8c78e50fefba42e797f4a3
Markus Korn <thekorn@gmx.de>
2010-09-23 13:05:07 +0200

    added some debug statement

--- bug_testrunner.py
+++ bug_testrunner.py
@@ -6,4 +6,5 @@ tests = map(lambda x: os.path.basename(x).rsplit(".", 1)[0], glob.glob("test/*.r
 tests += map(lambda x: os.path.basename(x).rsplit(".", 1)[0], glob.glob("test/*-test.py"))
 
 for test in tests:
+    print "*****************", test
     Popen(["test/run-all-tests.py", test, "blacklist-test"]).wait()
