e8e92a56ff2041636181ece833b59e653c9baa81
mikkel.kamstrup@canonical.com <>
2010-11-09 13:17:31 +0100

    Add Canonical Ltd to list of copyright holders since a lot of the files content was derived from libzeitgeist

--- zeitgeist/mimetypes.py
+++ zeitgeist/mimetypes.py
@@ -3,6 +3,7 @@
 # Zeitgeist
 #
 # Copyright © 2010 Markus Korn <thekorn@gmx.de>
+#             2010 Canonical Ltd
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
