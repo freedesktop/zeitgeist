55108cc67597fa77d405ce375da151f6b31001c5
Seif Lotfy <seif@lotfy.com>
2011-08-22 00:55:21 +0200

    add missing js file

---
+++ test/data/twenty_events.js
@@ -0,0 +1,327 @@
+[
+	{
+		"timestamp" : 100,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "firefox",
+		"origin" : "origin2",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Document",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/plain",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 101,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "icedove",
+		"origin" : "origin1",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Video",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/booboo",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 102,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "firefox",
+		"origin" : "origin1",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Document",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/looloo",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 103,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "icedove",
+		"origin" : "origin3",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Video",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/looloo",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 104,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "firefox",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Document",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/plain",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 106,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "firefox",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Video",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/plain",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 107,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "firefox",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Document",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/booboo",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 108,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "firefox",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Music",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/plain",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 109,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "firefox",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Music",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/plain",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 110,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "firefox",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Document",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/looloo",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 111,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "icedove",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Document",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/plain",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 112,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "firefox",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Document",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/plain",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 113,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "firefox",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Document",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/plain",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 114,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "icedove",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Music",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/wumbo",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 115,
+		"interpretation" : "stfu:EvilEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "firefox",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Document",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/plain",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 116,
+		"interpretation" : "stfu:ShoesEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "firefox",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Music",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/plain",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 117,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "firefox",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Document",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///home",
+				"mimetype" : "text/plain",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 119,
+		"interpretation" : "stfu:EvilEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "firefox",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Document",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///etc",
+				"mimetype" : "text/plain",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 118,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "firefox",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Something",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///home",
+				"mimetype" : "text/plain",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	},{
+		"timestamp" : 105,
+		"interpretation" : "stfu:OpenEvent",
+		"manifestation" : "stfu:UserActivity",
+		"actor" : "frobnicator",
+		"subjects" : [
+			{
+				"uri" : "file:///tmp/foo.txt",
+				"interpretation" : "stfu:Document",
+				"manifestation" : "stfu:File",
+				"origin" : "file:///tmp",
+				"mimetype" : "text/plain",
+				"text" : "this item has not text... rly!",
+				"storage" : "368c991f-8b59-4018-8130-3ce0ec944157"
+			}
+		]
+	}
+]
