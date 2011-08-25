e889f82dd1a498aa5acef3f7e008ccc878c16086
Seif Lotfy <seif@lotfy.com>
2011-08-25 02:09:26 +0200

    add monitor tests and some basic insert tests

--- test/dbus/remote-test.py
+++ test/dbus/remote-test.py
@@ -60,6 +60,199 @@ class ZeitgeistRemoteAPITest(testutils.RemoteTestCase):
 		retrieved_events = self.getEventsAndWait(ids)
 		self.assertEquals(1, len(retrieved_events))
 		self.assertEventsEqual(retrieved_events[0], events[0])
+	
+	def testFindTwoOfThreeEvents(self):
+		ev1 = Event.new_for_values(timestamp=400,
+					interpretation=Interpretation.ACCESS_EVENT,
+					manifestation=Manifestation.USER_ACTIVITY,
+					actor="Boogaloo")	
+		ev2 = Event.new_for_values(timestamp=500,
+					interpretation=Interpretation.ACCESS_EVENT,
+					manifestation=Manifestation.USER_ACTIVITY,
+					actor="Boogaloo")
+		ev3 = Event.new_for_values(timestamp=600,
+					interpretation=Interpretation.SEND_EVENT,
+					manifestation=Manifestation.USER_ACTIVITY,
+					actor="Boogaloo")
+		subj1 = Subject.new_for_values(uri="foo://bar",
+					interpretation=Interpretation.DOCUMENT,
+					manifestation=Manifestation.FILE_DATA_OBJECT)
+		subj2 = Subject.new_for_values(uri="foo://baz",
+					interpretation=Interpretation.IMAGE,
+					manifestation=Manifestation.FILE_DATA_OBJECT)
+		subj3 = Subject.new_for_values(uri="foo://quiz",
+					interpretation=Interpretation.AUDIO,
+					manifestation=Manifestation.FILE_DATA_OBJECT)
+		ev1.append_subject(subj1)
+		ev2.append_subject(subj1)
+		ev2.append_subject(subj2)
+		ev3.append_subject(subj2)
+		ev3.append_subject(subj3)
+		ids = self.insertEventsAndWait([ev1, ev2, ev3])
+		self.assertEquals(3, len(ids))
+		
+		events = self.getEventsAndWait(ids)
+		self.assertEquals(3, len(events))		
+		for event in events:
+			self.assertTrue(isinstance(event, Event))
+			self.assertEquals(Manifestation.USER_ACTIVITY, event.manifestation)
+			self.assertEquals("Boogaloo", event.actor)
+		
+		# Search for everything
+		ids = self.findEventIdsAndWait([], num_events=3)
+		self.assertEquals(3, len(ids)) # (we can not trust the ids because we don't have a clean test environment)
+		
+		# Search for some specific templates
+		subj_templ1 = Subject.new_for_values(manifestation=Manifestation.FILE_DATA_OBJECT)
+		subj_templ2 = Subject.new_for_values(interpretation=Interpretation.IMAGE)
+		event_template = Event.new_for_values(
+					actor="Boogaloo",
+					interpretation=Interpretation.ACCESS_EVENT,
+					subjects=[subj_templ1,subj_templ2])
+		ids = self.findEventIdsAndWait([event_template],
+						num_events=10)
+		self.assertEquals(1, len(ids))
+		
+	def testUnicodeInsert(self):
+		events = parse_events("test/data/unicode_event.js")
+		ids = self.insertEventsAndWait(events)
+		self.assertEquals(len(ids), len(events))
+		result_events = self.getEventsAndWait(ids)
+		self.assertEquals(len(ids), len(result_events))
+		
+	def testGetEvents(self):
+		events = parse_events("test/data/five_events.js")
+		ids = self.insertEventsAndWait(events) + [1000, 2000]
+		result = self.getEventsAndWait(ids)
+		self.assertEquals(len(filter(None, result)), len(events))
+		self.assertEquals(len(filter(lambda event: event is None, result)), 2)
+	
+	def testMonitorInsertEvents(self):
+		result = []
+		mainloop = self.create_mainloop()
+		tmpl = Event.new_for_values(interpretation="stfu:OpenEvent")
+		events = parse_events("test/data/five_events.js")
+		
+		def notify_insert_handler(time_range, events):
+			result.extend(events)
+			mainloop.quit()
+		
+		def notify_delete_handler(time_range, event_ids):
+			mainloop.quit()
+			self.fail("Unexpected delete notification")
+			
+		self.client.install_monitor(TimeRange.always(), [tmpl],
+			notify_insert_handler, notify_delete_handler)
+		self.client.insert_events(events)
+		mainloop.run()
+		
+		self.assertEquals(2, len(result))
+		
+	def testMonitorDeleteEvents(self):
+		result = []
+		mainloop = self.create_mainloop()
+		events = parse_events("test/data/five_events.js")
+		
+		def notify_insert_handler(time_range, events):
+			event_ids = map(lambda ev : ev.id, events)
+			self.client.delete_events(event_ids)
+		
+		def notify_delete_handler(time_range, event_ids):
+			mainloop.quit()
+			result.extend(event_ids)
+			
+			
+		self.client.install_monitor(TimeRange(125, 145), [],
+			notify_insert_handler, notify_delete_handler)
+		
+		self.client.insert_events(events)
+		mainloop.run()
+		
+		self.assertEquals(2, len(result))
+	
+	def testMonitorDeleteNonExistingEvent(self):
+		result = []
+		mainloop = self.create_mainloop(None)
+		events = parse_events("test/data/five_events.js")
+		
+		def timeout():
+			# We want this timeout - we should not get informed
+			# about deletions of non-existing events
+			mainloop.quit()
+			return False
+
+		def notify_insert_handler(time_range, events):
+			event_ids = map(lambda ev : ev.id, events)
+			self.client.delete_events([9999999])
+		
+		def notify_delete_handler(time_range, event_ids):
+			mainloop.quit()
+			self.fail("Notified about deletion of non-existing events %s", events)
+			
+		self.client.install_monitor(TimeRange(125, 145), [],
+			notify_insert_handler, notify_delete_handler)
+		
+		gobject.timeout_add_seconds(5, timeout)
+		self.client.insert_events(events)
+		mainloop.run()
+	
+	def testTwoMonitorsDeleteEvents(self):
+		result1 = []
+		result2 = []
+		mainloop = self.create_mainloop()
+		events = parse_events("test/data/five_events.js")
+		
+		def check_ok():
+			if len(result1) == 2 and len(result2) == 2:
+				mainloop.quit()
+
+		def notify_insert_handler1(time_range, events):
+			event_ids = map(lambda ev : ev.id, events)
+			self.client.delete_events(event_ids)
+		
+		def notify_delete_handler1(time_range, event_ids):
+			result1.extend(event_ids)
+			check_ok()
+		
+		def notify_delete_handler2(time_range, event_ids):
+			result2.extend(event_ids)
+			check_ok()
+			
+		self.client.install_monitor(TimeRange(125, 145), [],
+			notify_insert_handler1, notify_delete_handler1)
+		
+		self.client.install_monitor(TimeRange(125, 145), [],
+			lambda x, y: x, notify_delete_handler2)
+		
+		self.client.insert_events(events)
+		mainloop.run()
+		
+		self.assertEquals(2, len(result1))
+		self.assertEquals(2, len(result2))
+
+	def testMonitorInstallRemoval(self):
+		result = []
+		mainloop = self.create_mainloop()
+		tmpl = Event.new_for_values(interpretation="stfu:OpenEvent")
+		
+		def notify_insert_handler(notification_type, events):
+			pass
+		
+		def notify_delete_handler(time_range, event_ids):
+			mainloop.quit()
+			self.fail("Unexpected delete notification")
+		
+		mon = self.client.install_monitor(TimeRange.always(), [tmpl],
+			notify_insert_handler, notify_delete_handler)
+		
+		def removed_handler(result_state):
+			result.append(result_state)
+			mainloop.quit()
+		
+		self.client.remove_monitor(mon, removed_handler)
+		mainloop.run()
+		self.assertEquals(1, len(result))
+		self.assertEquals(1, result.pop())
 
 	def testInsertAndDeleteEvent(self):
 		# Insert an event
