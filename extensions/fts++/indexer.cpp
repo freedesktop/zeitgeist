6753fcbaab25b0c89e6a933f57db367327544ab6
Mikkel Kamstrup Erlandsen <mikkel.kamstrup@gmail.com>
2012-02-05 20:58:03 +0100

    Add yours truly as author and copyright holder in indexer.cpp

--- extensions/fts++/indexer.cpp
+++ extensions/fts++/indexer.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 Canonical Ltd
+ *               2012 Mikkel Kamstrup Erlandsen
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -14,6 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authored by Michal Hruby <michal.hruby@canonical.com>
+ *             Mikkel Kamstrup Erlandsen <mikkel.kamstrup@gmail.com>
  *
  */
 
