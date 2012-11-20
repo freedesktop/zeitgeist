850d5189d3159f7cab6c509a2dd58c9f427c0bfc
Seif Lotfy <seif@lotfy.com>
2012-11-20 16:43:27 +0100

    Add loop to the python example

--- examples/python/find_events.py
+++ examples/python/find_events.py
@@ -1,7 +1,8 @@
-from gi.repository import Zeitgeist
+from gi.repository import Zeitgeist, Gtk
 log = Zeitgeist.Log.get_default()
 
 def callback (x):
     print x
 
-log.get_events([x for x in xrange(100)], None, callback, None)  
+log.get_events([x for x in xrange(100)], None, callback, None)
+Gtk.main()
