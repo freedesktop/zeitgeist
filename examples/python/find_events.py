9d8da594240982622429cf2975756e9c8af5be89
Seif Lotfy <seif@lotfy.com>
2012-11-20 11:01:37 +0100

    Added missing callback function to the python example

--- examples/python/find_events.py
+++ examples/python/find_events.py
@@ -1,3 +1,7 @@
 from gi.repository import Zeitgeist
 log = Zeitgeist.Log.get_default()
-log.get_events([x for x in xrange(100)], None, callback, None)
+
+def callback (x):
+    print x
+
+log.get_events([x for x in xrange(100)], None, callback, None)  
